fd0c825afc0d030574b69b6bb18265e519f3dd0c
akihiro <marsstay07@gmail.com>
2024-09-15 16:19:05 +0900

    Home screen: polls list

---
+++ src/app/_layout.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import { View, Text, StatusBar } from 'react-native'
+import { Stack } from 'expo-router';
+
+export default function RootLayout() {
+  return <Stack />
+}
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,21 +1,39 @@
 import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { Stack } from 'expo-router';
+
+const polls = [1,2,3]
 
 export default function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>こんにちは。</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <Stack.Screen　options={{ title: 'Polls' }} />
+      <FlatList
+        data={polls}
+        contentContainerStyle={styles.container}
+        renderItem={({ item }) => (
+          <View style={styles.pollContainer}>
+            <Text style={styles.pollTitle}>{`${item}: Example pll question`}</Text>
+          </View>
+        )}
+      />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 10,
+    gap: 5
   },
+  pollContainer: {
+    backgroundColor: '#fff',
+    padding: 10,
+    borderRadius: 10,
+  },
+  pollTitle: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  }
 });

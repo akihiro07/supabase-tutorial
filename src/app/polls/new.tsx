bff1f62bcf9305fa26b32aa781ea4d8198ec82ea
akihiro <marsstay07@gmail.com>
2024-09-17 09:24:33 +0900

    Create polls

--- src/app/polls/new.tsx
+++ src/app/polls/new.tsx
@@ -1,14 +1,41 @@
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native'
+import { StyleSheet, Text, View, TextInput, Button, Alert } from 'react-native'
 import React, { useState } from 'react'
-import { Redirect, Stack } from 'expo-router';
+import { Redirect, router, Stack } from 'expo-router';
 import { Feather } from '@expo/vector-icons';
 import { useAuth } from '../../providers/AuthProvider';
+import { supabase } from '../../lib/supabase';
 
 const CreatePoll = () => {
   const [question, setQuestion] = useState('');
   const [options, setOptions] = useState(['', '']);
+  const [error, setError] = useState('');
 
-  const createPoll = () => {};
+  const createPoll = async () => {
+    setError('');
+
+    if(!question) {
+      setError('Please provide the question');
+      return;
+    }
+    const validOptions = options.filter(o => !!o);
+    if (validOptions.length < 2) {
+      setError('Please provide at least 2 vallid options');
+      return;
+    }
+
+
+    const { data, error } = await supabase
+      .from('polls')
+      .insert([{ question, options: validOptions }])
+      .select();
+    if (error) {
+      Alert.alert('Failed to create the poll');
+      console.error(error);
+      return;
+    };
+
+    router.back();
+  };
 
   const { user } = useAuth();
 
@@ -28,7 +55,7 @@ const CreatePoll = () => {
         onChangeText={setQuestion}
       />
 
-      <Text style={styles.label}>Title</Text>
+      <Text style={styles.label}>Options</Text>
       {options.map((option, index) => (
         <View key={index} style={{ justifyContent: 'center' }}>
           <TextInput
@@ -59,6 +86,7 @@ const CreatePoll = () => {
       <Button title='Add option' onPress={() => setOptions([...options, ''])} />
 
       <Button title='Create poll' onPress={createPoll} />
+      {error && <Text style={{ color: 'crimson' }}>{error}</Text>}
     </View>
   )
 }
